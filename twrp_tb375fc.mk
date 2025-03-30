9c3cd048dae59335a21bc55522ad8c21fdfe2ef7
luxwrty <lq2061125681@gmail.com>
2025-03-30 23:22:38 +0800

    first commit

---
+++ twrp_tb375fc.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from tb375fc device
+$(call inherit-product, device/lenovo/tb375fc/device.mk)
+
+# Configure launch_with_vendor_ramdisk.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+
+# Configure emulated_storage.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+PRODUCT_DEVICE := tb375fc
+PRODUCT_NAME := twrp_tb375fc
+PRODUCT_BRAND := Lenovo
+PRODUCT_MODEL := TB375FC
+PRODUCT_MANUFACTURER := Lenovo
+
+# Hide Reflash TWRP & FUSE passthrough
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.twrp.vendor_boot=true \
+    persist.sys.fuse.passthrough.enable=true
+    
