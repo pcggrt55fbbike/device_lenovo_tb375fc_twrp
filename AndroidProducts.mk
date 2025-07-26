225f0a35d29c7d4e21a4d53267600073dcc77ed6
pcggrt55fbbike <pcggrt55fbbike@gmail.com>
2025-07-26 10:03:35 +0900

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,19 +1,5 @@
 # device/lenovo/tb375fc/AndroidProducts.mk
 
-LOCAL_PATH := $(call my-dir)
-
-# このデバイス向けの Makefile を列挙
+# この1行でTWRP用のProduct定義を読み込みます
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/device.mk \
-    $(LOCAL_DIR)/BoardConfig.mk \
-    $(LOCAL_DIR)/ProjectConfig.mk
-
-# デバイス／ROM の識別子
-PRODUCT_DEVICE := tb375fc
-PRODUCT_BRAND  := lenovo
-PRODUCT_MODEL  := TB-375FC
-PRODUCT_MANUFACTURER := lenovo
-
-# TWRP のベース定義を取り込む
-inherit_vendor_bootimage := true
-$(call inherit-product, $(SRC_TARGET_DIR)/product/twrp_base.mk)
+    $(LOCAL_DIR)/twrp_tb375fc.mk
