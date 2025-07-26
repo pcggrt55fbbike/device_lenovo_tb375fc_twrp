d2c18b2041ae1385be7b618bd639454ccac51ecd
pcggrt55fbbike <pcggrt55fbbike@gmail.com>
2025-07-26 09:26:18 +0900

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,12 +1,19 @@
-#
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
+# device/lenovo/tb375fc/AndroidProducts.mk
 
+LOCAL_PATH := $(call my-dir)
+
+# このデバイス向けの Makefile を列挙
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_tb375fc.mk
+    $(LOCAL_DIR)/device.mk \
+    $(LOCAL_DIR)/BoardConfig.mk \
+    $(LOCAL_DIR)/ProjectConfig.mk
 
-COMMON_LUNCH_CHOICES := \
-    twrp_tb375fc-eng
+# デバイス／ROM の識別子
+PRODUCT_DEVICE := tb375fc
+PRODUCT_BRAND  := lenovo
+PRODUCT_MODEL  := TB-375FC
+PRODUCT_MANUFACTURER := lenovo
+
+# TWRP のベース定義を取り込む
+inherit_vendor_bootimage := true
+$(call inherit-product, $(SRC_TARGET_DIR)/product/twrp_base.mk)
