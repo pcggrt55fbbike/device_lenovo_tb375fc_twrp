994be206e2e3f0d6ec1551e625e928fcc02790f5
pcggrt55fbbike <pcggrt55fbbike@gmail.com>
2025-07-26 12:41:45 +0900

    Update device.mk

--- device.mk
+++ device.mk
@@ -14,17 +14,16 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.product.name=ZUI_16_TB373FU \
     ro.build.fingerprint=lenovo/ZUI_16_TB373FU/ZUI:13/SP1A.210812.003/230124:user/release-keys
 
-# Add custom props to avoid build errors
+# Custom properties to avoid build errors
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp \
     persist.vendor.radio.multisim.config=dsds \
     persist.sys.locale=ja-JP
 
-# Include kernel and DTB
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/kernel/Image.gz
+# Only include DTB (no kernel)
 BOARD_PREBUILT_DTB := $(DEVICE_PATH)/dtb.img
 
-# Recovery
+# Recovery configuration (vendor_boot integration)
 BOARD_VENDOR_RAMDISK_RECOVERY := true
 BOARD_INCLUDE_RECOVERY_RAMDISK_IN_VENDOR_BOOT := true
 
@@ -36,7 +35,7 @@ TW_SCREEN_BLANK_ON_BOOT := true
 TW_NO_USB_STORAGE := false
 TW_INPUT_BLACKLIST := "hbtp_vm"
 
-# File systems
+# File system types
 BOARD_SYSTEMIMAGE_PARTITION_TYPE := ext4
 BOARD_VENDORIMAGE_PARTITION_TYPE := ext4
 BOARD_USERDATAIMAGE_PARTITION_TYPE := f2fs
