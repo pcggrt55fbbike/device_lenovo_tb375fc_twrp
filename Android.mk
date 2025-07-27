52e8382c005ff9f779534bacb168fe59aa038f55
pcggrt55fbbike <pcggrt55fbbike@gmail.com>
2025-07-27 13:37:43 +0900

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,12 +1,14 @@
 #
 # Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
-#
 # SPDX-License-Identifier: Apache-2.0
 #
 
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),tb375fc)
-include $(call all-subdir-makefiles,$(LOCAL_PATH))
+    include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
+    # Add lunch combos for TWRP build targets
+    add_lunch_combo twrp_tb375fc-eng
+    add_lunch_combo twrp_tb375fc-userdebug
 endif
