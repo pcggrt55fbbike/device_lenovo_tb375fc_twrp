632d069a7cb2ff358e4815317b4fd6133a52c7a1
pcggrt55fbbike <pcggrt55fbbike@gmail.com>
2025-07-27 14:21:22 +0900

    Update twrp_tb375fc.mk

--- twrp_tb375fc.mk
+++ twrp_tb375fc.mk
@@ -1,56 +1,34 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
 # SPDX-License-Identifier: Apache-2.0
+#
 
-# Inherit AOSP base & TWRP config
+# Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Inherit some common Omni stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit from device-specific configuration
+# Inherit from tb375fc device
 $(call inherit-product, device/lenovo/tb375fc/device.mk)
 
-# Virtual A/B and storage
+# Configure launch_with_vendor_ramdisk.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+
+# Configure emulated_storage.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
-# Device identity
 PRODUCT_DEVICE := tb375fc
 PRODUCT_NAME := twrp_tb375fc
 PRODUCT_BRAND := Lenovo
 PRODUCT_MODEL := TB375FC
 PRODUCT_MANUFACTURER := Lenovo
 
-# Recovery variant and UI
-RECOVERY_VARIANT := twrp
-TW_DEVICE_VERSION := TB375FC-on-ZUI-16.0
-TW_THEME := portrait_hdpi
-TW_EXTRA_LANGUAGES := true
-TW_USE_MODEL_HARDWARE_ID := true
-TW_DEFAULT_EXTERNAL_STORAGE := true
-TW_INCLUDE_FB2PNG := true
-TW_INPUT_BLACKLIST := "hbtp_vm"
-TW_INCLUDE_CRYPTO := true
-TW_INCLUDE_FBE := true
-TW_INCLUDE_FBE_METADATA_DECRYPT := true
-RECOVERY_SDCARD_ON_DATA := true
-TW_TARGET_USES_QCOM_BSP := false
-
-# Partition definitions
-BOARD_VENDOR_BOOTIMAGE_PARTITION := vendor_boot
-BOARD_RECOVERYIMAGE_PARTITION := vendor_boot
-
-# Vendor_boot build declaration
-PRODUCT_BUILD_VENDOR_BOOT_IMAGE := true
-PRODUCT_VENDOR_RAMDISK_RECOVERY := true
-PRODUCT_VENDOR_BOOTIMAGE_PARTITION_SIZE := 67108864
-
-# Prebuilt DTB (if needed)
-DEVICE_PATH := device/lenovo/tb375fc
-BOARD_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb
-
-# OTA assertions (dual identity support)
-PRODUCT_OTA_ASSERT_DEVICE := tb373fu,tb375fc
-
-# System properties
+# Hide Reflash TWRP & FUSE passthrough
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.twrp.vendor_boot=true \
     persist.sys.fuse.passthrough.enable=true
+    
