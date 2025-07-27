81787ab76d09e32ac28cb1b932b0ddaf85488753
pcggrt55fbbike <pcggrt55fbbike@gmail.com>
2025-07-27 14:32:14 +0900

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,14 +1,10 @@
-#
-# Copyright (C) 2024 The Android Open Source Project
-# SPDX-License-Identifier: Apache-2.0
-#
-
 LOCAL_PATH := $(call my-dir)
 
+# Unconditionally enable lunch combos
+add_lunch_combo twrp_tb375fc-eng
+add_lunch_combo twrp_tb375fc-userdebug
+
+# Subdir inclusion (optional)
 ifeq ($(TARGET_DEVICE),tb375fc)
     include $(call all-subdir-makefiles,$(LOCAL_PATH))
-
-    # Add lunch combos for TWRP build targets
-    add_lunch_combo twrp_tb375fc-eng
-    add_lunch_combo twrp_tb375fc-userdebug
 endif
