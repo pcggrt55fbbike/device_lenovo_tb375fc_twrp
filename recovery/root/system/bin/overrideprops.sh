4eba23f6319e073c015486c11365abb360fb0e1a
pcggrt55fbbike <pcggrt55fbbike@gmail.com>
2025-07-31 10:50:40 +0900

    Update overrideprops.sh

--- recovery/root/system/bin/overrideprops.sh
+++ recovery/root/system/bin/overrideprops.sh
@@ -1,10 +1,17 @@
 #!/system/bin/sh
 
-resetprop ro.build.date.utc 0000000000
-resetprop ro.system.build.date.utc 0000000000
-resetprop ro.system_ext.build.date.utc 0000000000
-resetprop ro.vendor.build.date.utc 0000000000
-resetprop ro.odm.build.date.utc 0000000000
-resetprop ro.product.build.date.utc 0000000000
+# 日付偽装（そのまま）
+setprop ro.build.date.utc 0000000000
+setprop ro.system.build.date.utc 0000000000
+setprop ro.system_ext.build.date.utc 0000000000
+setprop ro.vendor.build.date.utc 0000000000
+setprop ro.odm.build.date.utc 0000000000
+setprop ro.product.build.date.utc 0000000000
+
+# decrypt関連のprop誘導（fstab選択用）
+setprop ro.hardware mt6897
+setprop ro.boot.hardware mt6897
+setprop ro.product.device TB375FC
+setprop ro.vendor.mediatek.platform MT6897
 
 exit 0
