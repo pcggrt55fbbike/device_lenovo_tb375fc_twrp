b302e2ba8e359223c5227e944003e0ae2440ccb0
pcggrt55fbbike <pcggrt55fbbike@gmail.com>
2025-10-08 01:07:22 +0900

    Update Android.mk

--- mtk_fbe_decrypt/Android.mk
+++ mtk_fbe_decrypt/Android.mk
@@ -2,8 +2,10 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := mtk_fbe_decrypt
-LOCAL_SRC_FILES := decrypt.cpp
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_SRC_FILES := decrypt.cpp hkdf_extract.cpp aes_gcm_decrypt.cpp
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include $(TOP)/external/openssl/include
+LOCAL_CPPFLAGS += -std=c++11
+LOCAL_SHARED_LIBRARIES := libcrypto
+LOCAL_LDLIBS += -lcrypto
 LOCAL_MODULE_TAGS := optional
-LOCAL_STATIC_LIBRARIES := libcrypto
 include $(BUILD_EXECUTABLE)
