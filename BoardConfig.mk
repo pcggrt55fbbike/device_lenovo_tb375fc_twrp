f9bce08a1a316562221ba514b73e561d9fd433e5
pcggrt55fbbike <pcggrt55fbbike@gmail.com>
2025-10-11 02:16:23 +0900

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -100,7 +100,7 @@ TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
 TARGET_COPY_OUT_ODM_DLKM := odm_dlkm
 
 # Decrypt対応バイナリ（gatekeeperd / keystore2 / init/ mtk_fbe_decrypt）
-TARGET_RECOVERY_DEVICE_MODULES := gatekeeperd keystore2 libinit_tb375fc mtk_fbe_decrypt
+TARGET_RECOVERY_DEVICE_MODULES := gatekeeperd keystore2 libinit_tb373fu mtk_fbe_decrypt
 
 # FBE decrypt構成
 TW_INCLUDE_CRYPTO := true
